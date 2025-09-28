c5d6fd5d671fbd6306103f0df3a444ddf9eb677b
bitka-de <behrens.ka@icloud.com>
2025-09-28 14:47:26 +0200

    feat: added Frontcontroler

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
 <?php
 
-// Entry point for the Brick Framework
+/**
+ * Front Controller for the Brick Framework
+ *
+ * @author  Jan P. Behrens <jp@bitka.de>
+ * @version 1.0
+ */
+
+require_once __DIR__ . '/../brick/Core/bootstrap.php';
