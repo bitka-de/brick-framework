a69a3fcc4e6551a46ee30ed178041eae12e7531f
bitka-de <behrens.ka@icloud.com>
2025-09-28 13:57:33 +0200

    feat(core): added autoloader (PSR-4)

--- brick/Core/Autoloader.php
+++ brick/Core/Autoloader.php
@@ -2,7 +2,59 @@
 
 namespace Brick\Core;
 
+/**
+ * Autoloader.php
+ *
+ * Ein minimalistischer PSR-4 Autoloader für Framework und App.
+ * Registriert Namespaces und lädt Klassen bei Bedarf.
+ *
+ * @package Brick\Core
+ *
+ * @author  Jan P. Behrens <jp@bitka.de>
+ * @version 1.0
+ */
+
 class Autoloader
 {
-    // Autoloader implementation
+  private array $prefixes = [];
+
+  /**
+   * Fügt einen Namespace-Präfix und das zugehörige Basisverzeichnis hinzu.
+   *
+   * @param string $prefix  Der Namespace-Präfix (z.B. "App\").
+   * @param string $baseDir Das Basisverzeichnis für diesen Präfix.
+   */
+  public function addNamespace(string $prefix, string $baseDir): void
+  {
+    $prefix = trim($prefix, '\\') . '\\';
+    $baseDir = rtrim($baseDir, DIRECTORY_SEPARATOR) . '/';
+    $this->prefixes[$prefix] = $baseDir;
+  }
+
+  /**
+   * Registriert den Autoloader bei SPL.
+   */
+  public function register(): void
+  {
+    spl_autoload_register([$this, 'loadClass']);
+  }
+
+  /**
+   * Lädt die Klasse basierend auf dem Namespace-Präfix.
+   *
+   * @param string $class Der vollqualifizierte Klassenname.
+   */
+  private function loadClass(string $class): void
+  {
+    foreach ($this->prefixes as $prefix => $baseDir) {
+      if (str_starts_with($class, $prefix)) {
+        $relative = substr($class, strlen($prefix));
+        $file = $baseDir . str_replace('\\', '/', $relative) . '.php';
+        if (file_exists($file)) {
+          require $file;
+          return;
+        }
+      }
+    }
+  }
 }
