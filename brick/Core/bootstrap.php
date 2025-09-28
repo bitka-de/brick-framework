01f2bd6d61cf080bbf93acdf63d4cc4866513174
bitka-de <behrens.ka@icloud.com>
2025-09-28 14:45:12 +0200

    feat(core): added bootstrap

--- brick/Core/bootstrap.php
+++ brick/Core/bootstrap.php
@@ -1,3 +1,22 @@
 <?php
 
-// Bootstrap file for initializing the Brick Framework
+/**
+ * bootstrap.php
+ *
+ * Bootstrap-Datei für die Initialisierung des Brick Frameworks.
+ * 
+ * @author  Jan P. Behrens <jp@bitka.de>
+ * @version 1.0
+ */
+ 
+// Autoloader einbinden
+require_once __DIR__ . '/Autoloader.php';
+
+// Autoloader initialisieren
+$autoloader = new Brick\Core\Autoloader();
+$autoloader->addNamespace('App', __DIR__ . '/../app');  // App Namespace
+$autoloader->addNamespace('Brick', __DIR__);            // Core Namespace
+$autoloader->register();
+
+// Weitere Initialisierungen
+echo 'Hallo Brick Framework!';
